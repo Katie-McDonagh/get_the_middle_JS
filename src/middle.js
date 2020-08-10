51e1b6c19967f4a695b7c3c968628c310d73e294
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-10 19:16:26 +0100

    refactor

--- src/middle.js
+++ src/middle.js
@@ -3,9 +3,9 @@ function getMiddle(str){
 
 
   if(str.length % 2 === 1) {
-  let middleNumber = Math.ceil(str.length /2)
+  var middleNumber = Math.ceil(str.length /2)
   return str.charAt(middleNumber -1);
   } else {
-    return "of"
-  }
+    return str.substring(middleNumber -1, middleNumber);
+  };
 };
